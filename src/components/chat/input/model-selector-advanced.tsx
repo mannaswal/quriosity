4f75325efb16ae82deb6cf020157fe709ab48a1a
Mann Aswal <mann1aswal@gmail.com>
2025-06-18 17:37:24 -0400

    Fixed runtime issue, I hope

--- src/components/chat/input/model-selector-advanced.tsx
+++ src/components/chat/input/model-selector-advanced.tsx
@@ -78,7 +78,12 @@ export const ModelSelectorAdvanced = ({
 						)}>
 						{currentModelData && (
 							<div className="size-4">
-								<currentModelProviderLogo.monochrome className="text-muted-foreground size-4 shrink-0" />
+								{(() => {
+									const LogoComponent = currentModelProviderLogo.monochrome;
+									return (
+										<LogoComponent className="text-muted-foreground size-4 shrink-0" />
+									);
+								})()}
 							</div>
 						)}
 						{currentModelData ? currentModelData.name : 'Select model'}
@@ -115,9 +120,12 @@ export const ModelSelectorAdvanced = ({
 										iconClassName="text-muted-foreground opacity-50 -mr-1">
 										<div className="flex items-center gap-2.5">
 											<div className="size-4">
-												{
-													<modelProviderLogo.monochrome className="text-muted-foreground size-4 shrink-0" />
-												}
+												{/* {(() => {
+													const LogoComponent = modelProviderLogo.monochrome;
+													return (
+														<LogoComponent className="text-muted-foreground size-4 shrink-0" />
+													);
+												})()} */}
 											</div>
 											{providerModelNames[provider as ModelProvider]}
 										</div>
